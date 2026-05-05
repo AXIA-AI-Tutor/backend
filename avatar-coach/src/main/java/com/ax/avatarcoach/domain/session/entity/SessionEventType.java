6a561cf3995510534ed3f60f8b61e0d5816e7ee6
신은찬 <dmscks3126@gmail.com>
2026-05-05 17:01:43 +0900

    KAN-119 feat(session): SessionEvent 도메인 모델 구현

---
+++ avatar-coach/src/main/java/com/ax/avatarcoach/domain/session/entity/SessionEventType.java
@@ -0,0 +1,12 @@
+package com.ax.avatarcoach.domain.session.entity;
+
+public enum SessionEventType {
+    SESSION_CREATED,
+    SESSION_STARTED,
+    QUESTION_GENERATED,
+    ANSWER_SUBMITTED,
+    FEEDBACK_CREATED,
+    REPORT_CREATED,
+    SESSION_COMPLETED,
+    SESSION_FAILED
+}
